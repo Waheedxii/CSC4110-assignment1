bd6d514b7a917ba437feddbc8e61fdb05c0ba20a
Waheed <go3487@wayne.edu>
2022-02-01 00:26:16 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Waheedalam Laskar <go3487@wayne.edu> 
 #include<iostream>  
   
 using namespace std; 
